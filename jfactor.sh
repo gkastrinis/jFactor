1ad1dafc9d338609486e52493ec9d099dbfe6e10
George Kastrinis <george.kast@gmail.com>
2021-04-01 20:56:30 +0300

    Move filter string in jfactor.sh into a variable

--- jfactor.sh
+++ jfactor.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 BASE=build/out
+filter='\<A test8()V\>'
 rm -rf $BASE && mkdir $BASE
 souffle -j4 -F$BASE/../out_tmp -D$BASE logic/jfactor.dl
 if [ -s $BASE/ERROR.csv ] ; then echo "Errors encountered!!!" ; cat $BASE/ERROR.csv ; fi
-sort -V $BASE/OpcodeExt.csv | grep '\<A test8()V\>' > $BASE/OpcodeExt.facts
+sort -V $BASE/OpcodeExt.csv | grep "$filter" > $BASE/OpcodeExt.facts
 rm $BASE/OpcodeExt.csv
 for f in $BASE/*csv ; do
-  (echo $f ; sort -V $f | grep '\<A test8()V\>'; echo) >> $BASE/all.txt
+  (echo $f ; sort -V $f | grep "$filter"; echo) >> $BASE/all.txt
 done
