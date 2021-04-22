47c4011aa785381c3feb81eae7c0f1d32f6cc296
George Kastrinis <george.kast@gmail.com>
2021-04-22 05:37:47 +0300

    Use bash time in jfactor.sh

--- jfactor.sh
+++ jfactor.sh
@@ -3,7 +3,7 @@ BASE=build/out
 filter='\<A test17(I)I\>'
 rm -rf $BASE && mkdir $BASE
 echo "Souffle..."
-/usr/bin/time souffle -j4 -F$BASE/../out_tmp -D$BASE logic/jfactor.dl
+time souffle -j4 -F$BASE/../out_tmp -D$BASE logic/jfactor.dl
 if [ -s $BASE/ERROR.csv ] ; then echo "Errors encountered!!!" ; cat $BASE/ERROR.csv ; fi
 sort -V $BASE/OpcodeExt.csv | grep "$filter" > $BASE/OpcodeExt.facts
 rm $BASE/OpcodeExt.csv
