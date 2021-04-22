d55639c51eb58849a16d5a55c67c8203aff0a3bc
George Kastrinis <george.kast@gmail.com>
2021-04-22 09:40:22 +0300

    Remove deserialization logic for stack;
    Souffle can output such types directly

--- jfactor.sh
+++ jfactor.sh
@@ -4,7 +4,7 @@ filter='\<A test8()V\>'
 rm -rf $BASE && mkdir $BASE
 echo "Souffle..."
 time souffle -j4 -F$BASE/../out_tmp -D$BASE logic/jfactor.dl
-if [ -s $BASE/ERROR.csv ] ; then echo "Errors encountered!!!" ; cat $BASE/ERROR.csv ; fi
+if [ -s $BASE/ERROR.csv ] ; then echo -e "\nErrors encountered!!!" ; fi
 sort -V $BASE/OpcodeExt.csv | grep "$filter" > $BASE/OpcodeExt.facts
 rm $BASE/OpcodeExt.csv
 for f in $BASE/*csv ; do
