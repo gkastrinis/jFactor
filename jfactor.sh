57be5ab91ad912757861ad2e6b418cde3d7cc193
George Kastrinis <george.kast@gmail.com>
2021-04-24 02:25:16 +0300

    Report number of error encountered in jfactor.sh

--- jfactor.sh
+++ jfactor.sh
@@ -4,7 +4,10 @@ filter='\<A test6('
 rm -rf $BASE && mkdir $BASE
 echo "Souffle..."
 time souffle -j4 -F$BASE/../out_tmp -D$BASE logic/jfactor.dl
-if [ -s $BASE/ERROR.csv ] ; then echo -e "\nErrors encountered!!!" ; fi
+if [ -s $BASE/ERROR.csv ] ; then
+  echo -e "\nErrors encountered!!!"
+  wc -l $BASE/ERROR.csv
+fi
 sort -V $BASE/OpcodeExt.csv | grep "$filter" > $BASE/OpcodeExt.facts
 rm $BASE/OpcodeExt.csv
 for f in $BASE/*csv ; do
