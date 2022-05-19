8a8c894ce43ab8e26bdc1a16babd992bb89c95ca
George Kastrinis <george.kast@gmail.com>
2022-05-19 17:58:26 +0300

    Remove machine specific path in `run.sh`

--- rt/rel/run.sh
+++ rt/rel/run.sh
@@ -8,7 +8,7 @@ DB=gka-hackathon
 rai get-database $DB && rai delete-database $DB
 rai create-database $DB
 
-BASE_PATH="/Users/gkastrinis/Work/jFactor/build/out_tmp"
+BASE_PATH="$PWD/build/out_tmp"
 rai load-csv $DB "$BASE_PATH/bytecode.csv" -r bytecode --schema "idx:int"
 rai load-csv $DB "$BASE_PATH/alloc-type.csv" -r alloc_type
 rai load-csv $DB "$BASE_PATH/label.csv" -r label --schema "stmt_idx:int"
